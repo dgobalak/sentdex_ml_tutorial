755c656812a3cd0c55ad42111114997475976593
danielg02 <danielgobalakrishnan@gmail.com>
2021-03-28 03:25:00 -0400

    Tested data on last 30 days of dataset and graphed all data.

--- regression_intro.py
+++ regression_intro.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import quandl
 import math
+import datetime
 import numpy as np
 from sklearn import preprocessing, svm, model_selection
 from sklearn.linear_model import LinearRegression
 import time
+import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use('ggplot')
 
 # Feature - Input Variable(s)
 # Label - THe thing we're predicting
@@ -24,31 +28,53 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
-print(forecast_out) # 30; We'll be prediciting prices 30 days in advance
+# print(forecast_out) # 30; We'll be prediciting prices 30 days in advance
 
 # The label column will be the Adj Close column from forecast_out days ago
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
-
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 
 # Will help with training/testing but adds to processing time
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+y = np.array(df['label'])
 
 # 20% of data will be used as testing data
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 # Making classifier
 clf = LinearRegression(n_jobs=-1)
-start = time.time()
 # clf = svm.SVR()
 clf.fit(X_train, y_train)
-end = time.time()
-
 
 # Testing classifier
 accuracy = clf.score(X_test, y_test)
-print(accuracy)
+# print(accuracy)
 
-print(f'Elapsed Time: {end - start} sec')
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+# print(df.head())
+for i in forecast_set:
+    next_data = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    # Sets forecast column in row to the prediction. df.loc[] is the index for each row
+    df.loc[next_data] = [np.nan for _ in range(len(df.columns)-1)] + [i]   
+
+# print(df.head())
+# print(df.tail())
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show() 
